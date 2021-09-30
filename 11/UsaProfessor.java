59dc5353f0beda43ad2d63dd14d92a224d79ca0a
Jakelyny <jakelynysousa.a@gmail.com>
2021-09-30 08:41:52 -0300

    .

---
+++ 11/UsaProfessor.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+/*
+ *  Disciplina: Tecnologia de Orientação a Objetos
+ *  Professor: Jorge Luis Boeira Bavaresco
+ *  Aluna: Jakelyny Sousa de Araújo
+ *  
+ * */
+
+public class UsaProfessor {
+	
+	public static void main(String[] args) {
+		
+		Cidade c1 = new Cidade(Integer.parseInt(JOptionPane.showInputDialog("Informe o código cidade: ")), 
+				JOptionPane.showInputDialog("Informe o nome da cidade: "), JOptionPane.showInputDialog("Informe o uf: "));
+		
+		Professor pf1 = new Professor(Integer.parseInt(JOptionPane.showInputDialog("Informe o código: ")), 
+				JOptionPane.showInputDialog("Informe o nome: "), Integer.parseInt(JOptionPane.showInputDialog("Informe a carga horária: ")),
+				Double.parseDouble(JOptionPane.showInputDialog("Informe o valor por hora: ")), c1, 
+				JOptionPane.showInputDialog("Informe a titulação: "),Integer.parseInt(JOptionPane.showInputDialog("Informe o número de turmas: ")));
+		pf1.calculaSalario();
+		pf1.exibirDados();
+	}
+}
